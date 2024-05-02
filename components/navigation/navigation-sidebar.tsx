d01c72131d37611b72432e55bb91f3d8bba11b77
Khushal Mali <khushalmali8448@gmail.com>
2024-05-02 20:53:10 +0530

    added separator to sidebar

--- components/navigation/navigation-sidebar.tsx
+++ components/navigation/navigation-sidebar.tsx
@@ -2,6 +2,7 @@ import { currentProfile } from "@/lib/current-profile";
 import { db } from "@/lib/db";
 import { redirect } from "next/navigation";
 import { NavigationAction } from "./navigation-action";
+import { Separator } from "@/components/ui/separator";
 
 export const NavigationSidebar = async () => {
   const profile = await currentProfile();
@@ -23,6 +24,7 @@ export const NavigationSidebar = async () => {
   return (
     <div className="space-y-4 flex flex-col items-center h-full text-primary w-full dark:bg-[#1E1F22] py-3">
       <NavigationAction />
+      <Separator className="h-[2px] bg-zinc-300 dark:bg-zinc-700 rounded-md w-10 mx-auto" />
     </div>
   );
 };
