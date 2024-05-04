9f699331e2afb3f1b7b24f4bf199a143c9f3345a
Khushal Mali <khushalmali8448@gmail.com>
2024-05-04 20:05:16 +0530

    update

---
+++ components/providers/modal-provider.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import { CreateServerModal } from "@/components/modals/create-server-modal";
+import { useState, useEffect } from "react";
+
+export const ModalProvider = () => {
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  if (!isMounted) {
+    return null;
+  }
+
+  return (
+    <>
+      <CreateServerModal />;
+    </>
+  );
+};
---
+++ type.ts
@@ -0,0 +1,7 @@
+import type { Server, Member, Profile } from "@prisma/client";
+
+export type ServerWithMembersWithProfile = Server & {
+  members: (Member & {
+    profile: Profile;
+  })[];
+};
