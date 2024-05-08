6ac507e834ccab2d989d7caa9b2b81581e556c0c
Khushal Mali <khushalmali8448@gmail.com>
2024-05-09 01:33:04 +0530

    added delete server modal

--- components/providers/modal-provider.tsx
+++ components/providers/modal-provider.tsx
@@ -7,6 +7,7 @@ import { MembersModal } from "@/components/modals/members-modal";
 import { CreateServerModal } from "@/components/modals/create-server-modal";
 import { CreateChannelModal } from "@/components/modals/create-channel-modal";
 import { LeaveServerModal } from "@/components/modals/leave-server-modal";
+import { DeleteServerModal } from "@/components/modals/delete-server-modal";
 
 export const ModalProvider = () => {
   const [isMounted, setIsMounted] = useState(false);
@@ -27,6 +28,7 @@ export const ModalProvider = () => {
       <MembersModal />
       <CreateChannelModal />
       <LeaveServerModal />
+      <DeleteServerModal />
     </>
   );
 };
