bdf8b4da47d944310b62926bb01e40cf2f83c8e0
Khushal Mali <khushalmali8448@gmail.com>
2024-05-14 16:11:08 +0530

    added file upload

--- components/providers/modal-provider.tsx
+++ components/providers/modal-provider.tsx
@@ -10,6 +10,7 @@ import { LeaveServerModal } from "@/components/modals/leave-server-modal";
 import { DeleteServerModal } from "@/components/modals/delete-server-modal";
 import { DeleteChannelModal } from "@/components/modals/delete-channel-modal";
 import { EditChannelModal } from "@/components/modals/edit-channel-modal";
+import { MessageFileModal } from "@/components/modals/message-file-modal";
 
 export const ModalProvider = () => {
   const [isMounted, setIsMounted] = useState(false);
@@ -33,6 +34,7 @@ export const ModalProvider = () => {
       <DeleteServerModal />
       <DeleteChannelModal />
       <EditChannelModal />
+      <MessageFileModal />
     </>
   );
 };
